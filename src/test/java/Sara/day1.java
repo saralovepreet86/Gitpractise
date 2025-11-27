f91cb2d0a0d0886830941f0ca3663f387ea89e80
Lovepreet Kaur <saralovepreet86@gmail.com>
2025-11-27 10:20:36 +0530

    COMMITFIRST

---
+++ src/test/java/Sara/day1.java
@@ -0,0 +1,23 @@
+package Sara;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class day1 {
+
+
+            public static void main(String[] args) {
+                        Queue<Integer> queue = new LinkedList<>();
+
+                        // Add elements
+                        queue.add(10);
+                        queue.add(20);
+                        queue.add(30);
+
+                        // Display elements
+                        System.out.println("Queue elements:");
+                        for (int num : queue) {
+                            System.out.print(num + " ");
+                        }
+                    }
+                }
